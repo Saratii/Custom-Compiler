a20f5c853cfe7b7e57343c6e27f6f7c51bca3450
Sarati <friedchicken14821@gmail.com>
2023-07-30 17:21:59 -0500

    started expression lexer

--- src/lex.rs
+++ src/lex.rs
@@ -12,49 +12,95 @@ pub enum Expression {
     Bool(bool),
     Variable(String),
     I32(String),
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Box<Expression>),
+    Divide(Box<Expression>, Box<Expression>),
 }
 #[derive(PartialEq, Debug, Clone)]
-pub enum Type{
+pub enum Type {
     Bool,
     String,
     I32,
 }
 pub fn lex(tokens: Vec<Token>) -> Vec<Line> {
     let mut lines = vec![];
-    for i in 0..tokens.len(){
-        process_token(i, &tokens, &mut lines)
+    let mut i = 0;
+    while i < tokens.len() {
+        i = process_token(i, &tokens, &mut lines);
+        i += 1;
     }
     lines
 }
 
-fn process_token(i: usize, tokens: &Vec<Token>, lines: &mut Vec<Line>){
-    match &tokens[i]{
+fn process_token(index: usize, tokens: &Vec<Token>, lines: &mut Vec<Line>) -> usize {
+    let mut i = index;
+    match &tokens[i] {
         Token::Print => {
-            match &tokens[i+1]{
-                Token::String(expression) => lines.push(Line::Print(Expression::String(expression.to_string()))),
-                Token::VariableName(name) => lines.push(Line::Print(Expression::Variable(name.to_string()))),
+            i += 1;
+            match &tokens[i] {
+                Token::String(expression) => {
+                    lines.push(Line::Print(Expression::String(expression.to_string())))
+                }
+                Token::VariableName(name) => {
+                    lines.push(Line::Print(Expression::Variable(name.to_string())))
+                }
                 Token::EndParen => lines.push(Line::Print(Expression::String("".to_string()))),
-                Token::ConstantNumber(value) => lines.push(Line::Print(Expression::String(value.to_string()))),
-                Token::Boolean(value) => lines.push(Line::Print(Expression::String(value.to_string()))),
-                _ => println!("{}", &format!("Oopsie Woopsie: invalid token following print: {:?}", tokens[i+1])),
-            }   
+                Token::ConstantNumber(value) => {
+                    lines.push(Line::Print(Expression::String(value.to_string())))
+                }
+                Token::Boolean(value) => {
+                    lines.push(Line::Print(Expression::String(value.to_string())))
+                }
+                _ => println!(
+                    "{}",
+                    &format!(
+                        "Oopsie Woopsie: invalid token following print: {:?}",
+                        tokens[i]
+                    )
+                ),
+            }
         }
         Token::TypeBool => {
-            match &tokens[i+1]{
+            i += 1;
+            match &tokens[i] {
                 Token::VariableName(name) => {
-                    match &tokens[i+2]{
-                        Token::Boolean(expression) => lines.push(Line::DefineVariable(name.to_string(), Expression::Bool(expression.clone()), Type::Bool)),
-                        _ => println!("{}", &format!("Oopsie Woopsie: invalid token following a variable name: {:?}", tokens[i+1])),
+                    i += 1;
+                    match &tokens[i] {
+                        Token::Boolean(expression) => lines.push(Line::DefineVariable(
+                            name.to_string(),
+                            Expression::Bool(expression.clone()),
+                            Type::Bool,
+                        )),
+                        _ => println!(
+                            "{}",
+                            &format!(
+                                "Oopsie Woopsie: invalid token following a variable name: {:?}",
+                                tokens[i]
+                            )
+                        ),
                     }
                 }
-                _ => println!("{}", &format!("Oopsie Woopsie: invalid token following Bool: {:?}", tokens[i+1])),
+                _ => println!(
+                    "{}",
+                    &format!(
+                        "Oopsie Woopsie: invalid token following Bool: {:?}",
+                        tokens[i]
+                    )
+                ),
             }
         }
         Token::TypeString => {
-            match &tokens[i+1]{
+            i += 1;
+            match &tokens[i] {
                 Token::VariableName(name) => {
-                    match &tokens[i+2]{
-                        Token::String(expression) => lines.push(Line::DefineVariable(name.to_string(), Expression::String(expression.to_string()), Type::String)),
+                    i += 1;
+                    match &tokens[i] {
+                        Token::String(expression) => lines.push(Line::DefineVariable(
+                            name.to_string(),
+                            Expression::String(expression.to_string()),
+                            Type::String,
+                        )),
                         _ => {}
                     }
                 }
@@ -62,66 +108,99 @@ fn process_token(i: usize, tokens: &Vec<Token>, lines: &mut Vec<Line>){
             }
         }
         Token::TypeI32 => {
-            match &tokens[i+1]{
+            i += 1;
+            match &tokens[i] {
                 Token::VariableName(name) => {
-                    match &tokens[i+2]{
-                        Token::ConstantNumber(value) => lines.push(Line::DefineVariable(name.to_string(), Expression::I32(value.to_string()), Type::I32)),
-                        _ => {}
-                    }
+                    i += 1;
+                    let end_pos =
+                        tokens
+                            .iter()
+                            .enumerate()
+                            .skip(i)
+                            .find_map(|(i, token)| match token {
+                                Token::EndLine => Some(i),
+                                _ => None,
+                            });
+                    let (literal, literal_type) = lex_expression(&tokens[i..end_pos.unwrap()]);
+                    println!("i:{} end point: {:?}", i, end_pos);
+                    println!(
+                        "added line with name: {}, and value: {:?}",
+                        name.to_string(),
+                        literal
+                    );
+                    lines.push(Line::DefineVariable(name.to_string(), literal, literal_type));
+
+                    i = end_pos.unwrap();
                 }
                 _ => {}
             }
         }
         Token::WhileLoop => {
-            match &tokens[i+1]{
+            i += 1;
+            match &tokens[i] {
                 Token::Boolean(condition) => {
                     let mut while_loop_lines = vec![];
-                    let mut token_index = i + 2;
-                    while token_index < tokens.len(){
+                    let mut token_index = i + 1;
+                    while token_index < tokens.len() {
                         if tokens[token_index] == Token::EndLoop {
-                            break
+                            break;
                         }
                         process_token(token_index, tokens, &mut while_loop_lines);
                         token_index += 1;
                     }
-                    lines.push(Line::WhileLoop(Expression::Bool(*condition), while_loop_lines));
+                    lines.push(Line::WhileLoop(
+                        Expression::Bool(*condition),
+                        while_loop_lines,
+                    ));
                 }
-                  
+
                 _ => todo!(),
             }
         }
         Token::VariableName(name) => {
-            match &tokens[i-1]{
-                Token::TypeBool => {}
-                Token::TypeI32 => {}
-                Token::TypeString => {}
-                _ => {
-                    match &tokens[i+1]{
-                        Token::Boolean(value) => {
-                            lines.push(Line::DefineVariable(name.to_string(), Expression::Bool(*value), Type::Bool))
-                        }
-                        Token::String(value) => {
-                            lines.push(Line::DefineVariable(name.to_string(), Expression::String(value.to_string()), Type::String))
-                        }
-                        Token::ConstantNumber(value) => {
-                            lines.push(Line::DefineVariable(name.to_string(), Expression::I32(value.to_string()), Type::I32))
-                        }
-                        _ => {}
-                    } 
-                }
-            }
+            i += 1;
+            let end_pos =
+                tokens
+                    .iter()
+                    .enumerate()
+                    .skip(i)
+                    .find_map(|(i, token)| match token {
+                        Token::EndLine => Some(i),
+                        _ => None,
+                    });
+            let (literal , literal_type) = lex_expression(&tokens[i..end_pos.unwrap()]);
+            println!("i:{} end point: {:?}", i, end_pos);
+            println!(
+                "added line with name: {}, and value: {:?}",
+                name.to_string(),
+                literal
+            );
+            lines.push(Line::DefineVariable(name.to_string(), literal, literal_type));
+            i = end_pos.unwrap();
         }
-        Token::EndLoop => {
-            lines.push(Line::EndLoop)
-        }
+        Token::EndLoop => lines.push(Line::EndLoop),
         _ => {}
     }
+    i
 }
 
+fn lex_expression(tokens: &[Token]) -> (Expression, Type) {
+    if tokens.len() == 1 {
+        match &tokens[0] {
+            Token::ConstantNumber(value) => return (Expression::I32(value.to_string()), Type::I32),
+            Token::String(value) => return (Expression::String(value.to_string()), Type::String),
+            Token::Boolean(value) => return (Expression::Bool(value.clone()), Type::Bool),
+            _ => {}
+        }
+    }
+    (Expression::Bool(false), Type::Bool)
+}
 #[cfg(test)]
 mod test {
-    use crate::{lex::Expression, tokenize::Token};
-
+    use crate::{
+        lex::Expression,
+        tokenize::{MathOp, Token},
+    };
     use super::{lex, Line, Type};
     #[test]
     fn test_1() {
@@ -135,13 +214,21 @@ mod test {
         assert_eq!(actual, expected);
     }
     #[test]
-    fn bool_test(){
-        let actual = lex(vec![Token::TypeBool, Token::VariableName("peepaw".to_string()), Token::Boolean(true)]);
-        let expected = vec![Line::DefineVariable("peepaw".to_string(), Expression::Bool(true), Type::Bool)];
+    fn bool_test() {
+        let actual = lex(vec![
+            Token::TypeBool,
+            Token::VariableName("peepaw".to_string()),
+            Token::Boolean(true),
+        ]);
+        let expected = vec![Line::DefineVariable(
+            "peepaw".to_string(),
+            Expression::Bool(true),
+            Type::Bool,
+        )];
         assert_eq!(actual, expected);
     }
     #[test]
-    fn print_variable_test(){
+    fn print_variable_test() {
         let actual = lex(vec![
             Token::TypeBool,
             Token::VariableName("eee".to_string()),
@@ -150,11 +237,14 @@ mod test {
             Token::VariableName("eee".to_string()),
             Token::EndParen,
         ]);
-        let expected = vec![Line::DefineVariable("eee".to_string(), Expression::Bool(true), Type::Bool), Line::Print(Expression::Variable("eee".to_string()))];
+        let expected = vec![
+            Line::DefineVariable("eee".to_string(), Expression::Bool(true), Type::Bool),
+            Line::Print(Expression::Variable("eee".to_string())),
+        ];
         assert_eq!(actual, expected);
     }
     #[test]
-    fn print_string_test(){
+    fn print_string_test() {
         let actual = lex(vec![
             Token::TypeString,
             Token::VariableName("ee".to_string()),
@@ -164,13 +254,17 @@ mod test {
             Token::EndParen,
         ]);
         let expected = vec![
-            Line::DefineVariable("ee".to_string(), Expression::String("should I kill myself?".to_string()), Type::String),
+            Line::DefineVariable(
+                "ee".to_string(),
+                Expression::String("should I kill myself?".to_string()),
+                Type::String,
+            ),
             Line::Print(Expression::Variable("ee".to_string())),
-            ];
+        ];
         assert_eq!(actual, expected);
     }
     #[test]
-    fn simple_while_loop(){
+    fn simple_while_loop() {
         let actual = lex(vec![
             Token::WhileLoop,
             Token::Boolean(true),
@@ -182,38 +276,116 @@ mod test {
             Token::EndLoop,
         ]);
         let expected = vec![
-            Line::WhileLoop(Expression::Bool(true), vec![Line::Print(Expression::String("69".to_string()))]),
+            Line::WhileLoop(
+                Expression::Bool(true),
+                vec![Line::Print(Expression::String("69".to_string()))],
+            ),
             Line::Print(Expression::String("69".to_string())),
             Line::EndLoop,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
-    fn change_variable(){
+    fn change_variable() {
         let actual = lex(vec![
             Token::TypeI32,
             Token::VariableName("i".to_string()),
             Token::ConstantNumber("0".to_string()),
+            Token::EndLine,
             Token::VariableName("i".to_string()),
             Token::ConstantNumber("1".to_string()),
+            Token::EndLine,
             Token::TypeString,
             Token::VariableName("e".to_string()),
             Token::String("hello".to_string()),
+            Token::EndLine,
             Token::VariableName("e".to_string()),
             Token::String("bye".to_string()),
+            Token::EndLine,
             Token::TypeBool,
             Token::VariableName("yes".to_string()),
             Token::Boolean(true),
+            Token::EndLine,
             Token::VariableName("yes".to_string()),
             Token::Boolean(false),
+            Token::EndLine,
         ]);
         let expected = vec![
             Line::DefineVariable("i".to_string(), Expression::I32("0".to_string()), Type::I32),
             Line::DefineVariable("i".to_string(), Expression::I32("1".to_string()), Type::I32),
-            Line::DefineVariable("e".to_string(), Expression::String("hello".to_string()), Type::String),
-            Line::DefineVariable("e".to_string(), Expression::String("bye".to_string()), Type::String),
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::String("hello".to_string()),
+                Type::String,
+            ),
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::String("bye".to_string()),
+                Type::String,
+            ),
             Line::DefineVariable("yes".to_string(), Expression::Bool(true), Type::Bool),
             Line::DefineVariable("yes".to_string(), Expression::Bool(false), Type::Bool),
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    fn simple_math() {
+        let actual = lex(vec![
+            Token::TypeI32,
+            Token::VariableName("e".to_string()),
+            Token::ConstantNumber("4".to_string()),
+            Token::MathOp(MathOp::Add),
+            Token::ConstantNumber("3".to_string()),
+            Token::TypeI32,
+            Token::VariableName("ee".to_string()),
+            Token::ConstantNumber("4".to_string()),
+            Token::MathOp(MathOp::Subtract),
+            Token::ConstantNumber("3".to_string()),
+            Token::TypeI32,
+            Token::VariableName("eee".to_string()),
+            Token::ConstantNumber("8".to_string()),
+            Token::MathOp(MathOp::Divide),
+            Token::ConstantNumber("2".to_string()),
+            Token::TypeI32,
+            Token::VariableName("eeee".to_string()),
+            Token::ConstantNumber("8".to_string()),
+            Token::MathOp(MathOp::Multiply),
+            Token::ConstantNumber("2".to_string()),
+            Token::EndLine,
+        ]);
+        let expected = vec![
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::Add(
+                    Box::new(Expression::I32("4".to_string())),
+                    Box::new(Expression::I32("3".to_string())),
+                ),
+                Type::I32,
+            ),
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::Subtract(
+                    Box::new(Expression::I32("4".to_string())),
+                    Box::new(Expression::I32("3".to_string())),
+                ),
+                Type::I32,
+            ),
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::Divide(
+                    Box::new(Expression::I32("8".to_string())),
+                    Box::new(Expression::I32("2".to_string())),
+                ),
+                Type::I32,
+            ),
+            Line::DefineVariable(
+                "e".to_string(),
+                Expression::Multiply(
+                    Box::new(Expression::I32("8".to_string())),
+                    Box::new(Expression::I32("2".to_string())),
+                ),
+                Type::I32,
+            ),
         ];
         assert_eq!(actual, expected);
     }
--- src/tokenize.rs
+++ src/tokenize.rs
@@ -14,7 +14,16 @@ pub enum Token {
     ConstantNumber(String),
     Boolean(bool),
     WhileLoop,
-    Expression(String)
+    MathOp(MathOp),
+    EndLine,
+    OpenParen,
+}
+#[derive(PartialEq, Debug, Clone)]
+pub enum MathOp{
+    Add,
+    Multiply,
+    Divide,
+    Subtract,
 }
 
 pub fn parse_to_tokens(raw: &str) -> Vec<Token> {
@@ -76,6 +85,21 @@ pub fn parse_to_tokens(raw: &str) -> Vec<Token> {
         } else if inputs.starts_with("{"){
             tokens.push(Token::StartLoop);
             inputs = inputs[1..].to_string();
+        } else if inputs.starts_with(" + "){
+            tokens.push(Token::MathOp(MathOp::Add));
+            inputs = inputs[3..].to_string();
+        } else if inputs.starts_with(" - "){
+            tokens.push(Token::MathOp(MathOp::Subtract));
+            inputs = inputs[3..].to_string();
+        } else if inputs.starts_with(" * "){
+            tokens.push(Token::MathOp(MathOp::Multiply));
+            inputs = inputs[3..].to_string();
+        } else if inputs.starts_with(" / "){
+            tokens.push(Token::MathOp(MathOp::Divide));
+            inputs = inputs[3..].to_string();
+        } else if inputs.starts_with(";"){
+            tokens.push(Token::EndLine);
+            inputs = inputs[1..].to_string();
         } else if name_regex.is_match(&inputs) {
             let variable_name = name_regex
                 .captures(&inputs)
@@ -98,87 +122,98 @@ pub fn parse_to_tokens(raw: &str) -> Vec<Token> {
 #[cfg(test)]
 mod test {
 
+    use crate::tokenize::MathOp;
+
     use super::{parse_to_tokens, Token};
 
     #[test]
     fn test_1() {
-        let actual = parse_to_tokens("print(\"hello world\")");
+        let actual = parse_to_tokens("print(\"hello world\");");
         let expected = vec![
             Token::Print,
             Token::String("hello world".to_string()),
             Token::EndParen,
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn integer_variable_test() {
-        let actual = parse_to_tokens("i32 eeebo = 69");
+        let actual = parse_to_tokens("i32 eeebo = 69;");
         let expected = vec![
             Token::TypeI32,
             Token::VariableName("eeebo".to_string()),
             Token::ConstantNumber("69".to_string()),
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn string_variable_test() {
-        let actual = parse_to_tokens("String beebo = \"inhale carbon monoxide\"");
+        let actual = parse_to_tokens("String beebo = \"inhale carbon monoxide\";");
         let expected = vec![
             Token::TypeString,
             Token::VariableName("beebo".to_string()),
             Token::String("inhale carbon monoxide".to_string()),
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn bool_variable_test() {
-        let actual = parse_to_tokens("Bool feefoo = False");
+        let actual = parse_to_tokens("Bool feefoo = False;");
         let expected = vec![
             Token::TypeBool,
             Token::VariableName("feefoo".to_string()),
-            Token::Boolean(false)
+            Token::Boolean(false),
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn i32_variable_test() {
-        let actual = parse_to_tokens("i32 furfu = 420");
+        let actual = parse_to_tokens("i32 furfu = 420;");
         let expected = vec![
             Token::TypeI32,
             Token::VariableName("furfu".to_string()),
             Token::ConstantNumber("420".to_string()),
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn print_variable_test(){
-        let actual = parse_to_tokens("Bool eee = True\nprint(eee)");
+        let actual = parse_to_tokens("Bool eee = True;\nprint(eee);");
         let expected = vec![
             Token::TypeBool,
             Token::VariableName("eee".to_string()),
             Token::Boolean(true),
+            Token::EndLine,
             Token::Print,
             Token::VariableName("eee".to_string()),
             Token::EndParen,
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn print_string_variable(){
-        let actual = parse_to_tokens("String ee = \"should I kill myself?\"\nprint(ee)");
+        let actual = parse_to_tokens("String ee = \"should I kill myself?\";\nprint(ee);");
         let expected = vec![
             Token::TypeString,
             Token::VariableName("ee".to_string()),
             Token::String("should I kill myself?".to_string()),
+            Token::EndLine,
             Token::Print,
             Token::VariableName("ee".to_string()),
             Token::EndParen,
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn while_true(){
-        let actual = parse_to_tokens("while (True){\nprint(69)\n}");
+        let actual = parse_to_tokens("while (True){\nprint(69);\n}");
         let expected = vec![
             Token::WhileLoop,
             Token::Boolean(true),
@@ -187,6 +222,7 @@ mod test {
             Token::Print,
             Token::ConstantNumber("69".to_string()),
             Token::EndParen,
+            Token::EndLine,
             Token::EndLoop,
         ];
         assert_eq!(actual, expected);
@@ -194,15 +230,16 @@ mod test {
     #[test]
     fn format_tab(){
         let actual = parse_to_tokens("
-i32 e = 69
+i32 e = 69;
 while (True){
-    print(e)
+    print(e);
 }"
         );
         let expected = vec![
             Token::TypeI32,
             Token::VariableName("e".to_string()),
             Token::ConstantNumber("69".to_string()),
+            Token::EndLine,
             Token::WhileLoop,
             Token::Boolean(true),
             Token::EndParen,
@@ -210,29 +247,84 @@ while (True){
             Token::Print,
             Token::VariableName("e".to_string()),
             Token::EndParen,
+            Token::EndLine,
             Token::EndLoop,
         ];
         assert_eq!(actual, expected);
     }
     #[test]
     fn change_variable(){
-        let actual = parse_to_tokens("i32 i = 0\ni = 1\nString e = \"hello\"\ne = \"bye\"\nBool yes = True\nyes = False");
+        let actual = parse_to_tokens("i32 i = 0;\ni = 1;\nString e = \"hello\";\ne = \"bye\";\nBool yes = True;\nyes = False;");
         let expected = vec![
             Token::TypeI32,
             Token::VariableName("i".to_string()),
             Token::ConstantNumber("0".to_string()),
+            Token::EndLine,
             Token::VariableName("i".to_string()),
             Token::ConstantNumber("1".to_string()),
+            Token::EndLine,
             Token::TypeString,
             Token::VariableName("e".to_string()),
             Token::String("hello".to_string()),
+            Token::EndLine,
             Token::VariableName("e".to_string()),
             Token::String("bye".to_string()),
+            Token::EndLine,
             Token::TypeBool,
             Token::VariableName("yes".to_string()),
             Token::Boolean(true),
+            Token::EndLine,
             Token::VariableName("yes".to_string()),
             Token::Boolean(false),
+            Token::EndLine,
+        ];
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn simple_math(){
+        let actual = parse_to_tokens("i32 e = 4 + 3;\ni32 ee = 4 - 3;\ni32 eee = 8 / 2;\ni32 eeee = 8 * 2;");
+        let expected = vec![
+            Token::TypeI32,
+            Token::VariableName("e".to_string()),
+            Token::ConstantNumber("4".to_string()),
+            Token::MathOp(MathOp::Add),
+            Token::ConstantNumber("3".to_string()),
+            Token::EndLine,
+            Token::TypeI32,
+            Token::VariableName("ee".to_string()),
+            Token::ConstantNumber("4".to_string()),
+            Token::MathOp(MathOp::Subtract),
+            Token::ConstantNumber("3".to_string()),
+            Token::EndLine,
+            Token::TypeI32,
+            Token::VariableName("eee".to_string()),
+            Token::ConstantNumber("8".to_string()),
+            Token::MathOp(MathOp::Divide),
+            Token::ConstantNumber("2".to_string()),
+            Token::EndLine,
+            Token::TypeI32,
+            Token::VariableName("eeee".to_string()),
+            Token::ConstantNumber("8".to_string()),
+            Token::MathOp(MathOp::Multiply),
+            Token::ConstantNumber("2".to_string()),
+            Token::EndLine,
+        ];
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn multi_term_simple_math(){
+        let actual = parse_to_tokens("i32 foo = 3 + 5 / 4 * 68;");
+        let expected = vec![
+            Token::TypeI32,
+            Token::VariableName("foo".to_string()),
+            Token::ConstantNumber("3".to_string()),
+            Token::MathOp(MathOp::Add),
+            Token::ConstantNumber("5".to_string()),
+            Token::MathOp(MathOp::Divide),
+            Token::ConstantNumber("4".to_string()),
+            Token::MathOp(MathOp::Multiply),
+            Token::ConstantNumber("68".to_string()),
+            Token::EndLine,
         ];
         assert_eq!(actual, expected);
     }
